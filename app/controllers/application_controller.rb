f7510ef206ebe49d07cd5ef32b4ad8a06bb59396
Nic0b <nicob69@ymail.com>
2019-12-19 19:11:49 +0100

    fix: custom img notif

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+	skip_before_action :verify_authenticity_token
  $vapid_private = "48e-I288Qi9Caboi8enKfhffslSpx9Eas5OjsuETAXg="   
   $vapid_public = "BL9fMz34qqu63XxdheE663mRJRNuqqIL3vR19iqqrhq9XXo04ejetNSRw0yOG7tqsQZVU-jrGb8hYfeJZjhZAkY="
 
