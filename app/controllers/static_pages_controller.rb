ab7f32273ef709f0cc83eb8a0d7ef18831b98442
Nic0b <nicob69@ymail.com>
2019-12-19 20:32:52 +0100

    test: resolv notif icon route

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -23,15 +23,11 @@ def sendPush
 	end
 
 	def sendPayload(user)
-	    @message = {
-  title: "title",
-  body: "body",
-  icon: "https://webpratik.fr/wp-content/uploads/2019/01/logo_herde01-1-1004x1024.png"
-}
+	    @message = get_message(user.name)
 	    if user.notif_id.present?
 	      @notification_data = NotificationDatum.find(user.notif_id)
 	      Webpush.payload_send(endpoint: @notification_data.endpoint,
-	                           message: JSON.generate(@message),
+	                           message: @message,
 	                           p256dh: @notification_data.p256dh_key,
 	                           auth: @notification_data.auth_key,
 	                           ttl: 24 * 60 * 60,
